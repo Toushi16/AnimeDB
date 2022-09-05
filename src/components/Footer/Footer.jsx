19618110cd3e3632f168d996139996659f080d52
Toushi16 <domenicoseminara2000@hotmail.com>
2022-09-05 20:14:25 +0200

    fixed other card issues

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
             <div className='icon'>
             <i className="fa-brands fa-facebook"><a href="#"></a></i>
             <i className="fa-brands fa-instagram"><a href="#"></a></i>
-            <i class="fa-brands fa-twitter"><a href="#"></a></i>
-            <i class="fa-brands fa-github"><a href="#"></a></i>
+            <i className="fa-brands fa-twitter"><a href="#"></a></i>
+            <i className="fa-brands fa-github"><a href="#"></a></i>
             </div>
                 <h1>Made by Domenico</h1>
         </div>
